24c917c0906bd5485192838ceaa0c99e2a23410c
Divya Chaudhary <universechamp110@gmail.com>
2024-09-25 23:21:26 +0530

    ww

--- home.js
+++ home.js
@@ -1,4 +1,11 @@
-document.getElementById('cta-btn').addEventListener('click', function() {
-    alert("Welcome to CricX! Explore the world of cricket.");
+document.getElementById('cta-btn').addEventListener('click', function () {
+  alert("Welcome to CricX! Explore the world of cricket.");
+});
+
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('active');
   });
-  
+});
